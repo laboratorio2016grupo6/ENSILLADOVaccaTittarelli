5bb3f08d2fae9b2d37490f85da8732075d983de7
pisculichi <pisculichi@openmailbox.org>
2016-12-06 16:24:57 -0300

    First commit

---
+++ app/src/androidTest/java/laboratorio2016/github/com/ensilladovaccatittarelli/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package laboratorio2016.github.com.ensilladovaccatittarelli;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("laboratorio2016.github.com.ensilladovaccatittarelli", appContext.getPackageName());
+    }
+}
